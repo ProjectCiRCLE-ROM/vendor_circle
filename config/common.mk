dbf237d27a71510f4ba33388c237845e460230d1
kondors1995 <normandija1945@gmail.com>
2021-09-08 07:54:54 +0000

    vendor: Add pixel_2016_exclusive.xml
    * This allows us to use unlimited backup in Google Photos infinitely.
    
    Change-Id: I8b51142750745c28ba9c4cd1e5ca62876d54dce8

--- config/common.mk
+++ config/common.mk
@@ -90,6 +90,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.software.credentials.prebuilt.xml
 
+# Unlimited google photos backup
+PRODUCT_COPY_FILES += \
+    vendor/circle/prebuilt/google/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
+
 # Enable wireless Xbox 360 controller support
 PRODUCT_COPY_FILES += \
     frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:$(TARGET_COPY_OUT_PRODUCT)/usr/keylayout/Vendor_045e_Product_0719.kl
