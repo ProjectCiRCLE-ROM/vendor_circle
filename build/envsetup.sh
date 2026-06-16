6ee2a71a925bde7c59de599c7196c821d099c484
Bahar KURT <kurtbahartr@users.noreply.github.com>
2026-06-16 22:31:13 +0300

    build: Fix make target used to build the full package

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -32,7 +32,7 @@ function brunch()
 {
     breakfast $*
     if [ $? -eq 0 ]; then
-        mka bacon
+        mka live
     else
         echo "No such item in brunch menu. Try 'breakfast'"
         return 1
@@ -707,7 +707,7 @@ function cmka() {
     if [ ! -z "$1" ]; then
         for i in "$@"; do
             case $i in
-                bacon|otapackage|systemimage)
+                live|otapackage|systemimage)
                     mka installclean
                     mka $i
                     ;;
