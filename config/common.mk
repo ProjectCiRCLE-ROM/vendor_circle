f9619939d0f9ea760b105548feec117448e54419
Anirudh Gupta <anirudhgupta109@gmail.com>
2026-06-18 16:14:48 +0300

    vendor: fonts: Add Google Sans font and change output dir
    Change-Id: I7fd353cb2991caa1cb9b7b5dfed86225db44ef86
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -215,6 +215,9 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/%/libfuse-lite.so \
     system/%/libntfs-3g.so
 
+# Fonts
+include vendor/circle/config/fonts.mk
+
 # FRP
 PRODUCT_COPY_FILES += \
     vendor/circle/prebuilt/common/bin/wipe-frp.sh:$(TARGET_COPY_OUT_RECOVERY)/root/system/bin/wipe-frp
