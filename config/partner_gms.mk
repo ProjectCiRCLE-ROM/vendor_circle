285ead61a16d0c4d0e80e22caeb599b604aa7c39
Bahar KURT <kurtbahartr@users.noreply.github.com>
2026-06-19 04:26:21 +0300

    overlay: Initial overlay for swapping updater URL with GMS one

--- config/partner_gms.mk
+++ config/partner_gms.mk
@@ -5,4 +5,8 @@ $(call inherit-product, vendor/gms/config.mk)
 # Unlimited google photos backup
 PRODUCT_COPY_FILES += \
     vendor/circle/prebuilt/google/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
+
+# Updater overlay
+PRODUCT_PACKAGES += \
+    UpdaterOverlayGMS
 endif
