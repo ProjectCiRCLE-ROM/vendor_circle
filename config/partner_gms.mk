11a38896a4f39e5c0eabbd55e1ce0ef26b9de813
Bahar KURT <kurtbahartr@users.noreply.github.com>
2026-06-23 08:58:47 +0000

    config: Replace GMS repo
    halcyon GApps causes our wallpaper to be overridden with AOSP one
    somehow.

--- config/partner_gms.mk
+++ config/partner_gms.mk
@@ -1,6 +1,9 @@
 ifeq ($(WITH_GMS),true)
 # Inherit GMS repo
-$(call inherit-product, vendor/gms/config.mk)
+$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
+
+# Permission definitions
+$(call inherit-product, vendor/gapps/permissions/permissions.mk)
 
 # Unlimited google photos backup
 PRODUCT_COPY_FILES += \
@@ -9,4 +12,5 @@ PRODUCT_COPY_FILES += \
 # Updater overlay
 PRODUCT_PACKAGES += \
     UpdaterOverlayGMS
+
 endif
