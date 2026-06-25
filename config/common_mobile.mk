6639d96acb3539ebaf139a5354d5b4591476f444
Bahar KURT <kurtbahartr@users.noreply.github.com>
2026-06-25 14:03:13 +0300

    Revert "config: Remove WITH_GMS guards from AOSP/LineageOS apps"
    This reverts commit abc0ab6f03dc4f56c5d918c45025e201e1b5b32d.

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -18,9 +18,11 @@ PRODUCT_PACKAGES += \
     AvatarPicker \
     Backgrounds
 
+ifneq ($(WITH_GMS), true)
 PRODUCT_PACKAGES += \
     Glimpse \
     LatinIME
+endif
 
 ifeq ($(PRODUCT_TYPE), go)
 PRODUCT_PACKAGES += \
