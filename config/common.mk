2bf16d1b3f80b10887456e0e754615cb121178e4
Bahar KURT <kurtbahartr@users.noreply.github.com>
2026-07-09 18:55:06 +0300

    config: Add face unlock blobs to artifact path allowlist
    Fixes build on bluejay.

--- config/common.mk
+++ config/common.mk
@@ -184,6 +184,16 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
     ro.face.sense_service=true
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/etc/default-permissions/default_permissions_co.aospa.sense.xml \
+    system/etc/permissions/android.hardware.biometrics.face.xml \
+    system/etc/permissions/privapp_whitelist_co.aospa.sense.xml \
+    system/etc/sysconfig/hiddenapi-whitelist-co.aospa.sense.xml \
+    system/lib64/libFaceDetectCA.so \
+    system/lib64/libMegviiUnlock-jni-1.2.so \
+    system/lib64/libMegviiUnlock.so \
+    system/lib64/libmegface.so \
+    system/priv-app/ParanoidSense/ParanoidSense.apk
 endif
 
 # Filesystems tools
